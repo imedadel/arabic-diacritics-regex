e9f0f4323835cb429ee48598fea342f169f9d314
Imed Adel <aadelimed@gmail.com>
2019-06-16 22:47:16 +0200

    :bug: ava semicolons

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-import test from 'ava';
-import arabicDiacriticsRegex from '.';
+import test from 'ava'
+import arabicDiacriticsRegex from '.'
 
 test('title', t => {
 	t.throws(() => {
-		arabicDiacriticsRegex(123);
+		arabicDiacriticsRegex(123)
 	}, {
 		instanceOf: TypeError,
 		message: 'Expected a string, got number'
-	});
+	})
 
-	t.is(arabicDiacriticsRegex('unicorns'), 'unicorns & rainbows');
-});
+	t.is(arabicDiacriticsRegex('unicorns'), 'unicorns & rainbows')
+})
